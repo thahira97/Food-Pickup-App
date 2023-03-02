50edb74c3bf2dfd96f213075e544ad1f411039ea
Humaira Tasnim <hxtasnim@gmail.com>
2023-03-02 12:49:46 -0500

    Add menu page template and routes

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const menuRoutes = require('./routes/menu');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -40,6 +41,7 @@ app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
 // Note: mount other resources here, using the same pattern above
+app.use('/menu', menuRoutes);
 
 // Home page
 // Warning: avoid creating more routes in this file!
