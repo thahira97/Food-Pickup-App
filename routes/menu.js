16867c08e7a12ee3409f87227ab85da332717024
thahira97 <thahirasheerin3@gmail.com>
2023-03-02 19:11:17 -0500

    Render all the dishes from database based on category

--- routes/menu.js
+++ routes/menu.js
@@ -1,8 +1,14 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const db = require("../db/connection");
 
-router.get('/', (req, res) => {
-  res.render('menu');
+router.get("/", (req, res) => {
+  db
+   .query(`SELECT * FROM dishes;`)
+   .then((response) => {
+    console.log(response.rows)
+    res.render("menu", {dishes : response.rows});
+  });
 });
 
 module.exports = router;
