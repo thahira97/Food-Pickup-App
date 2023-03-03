663cc0da809d62a4785881a27eaff61808efcf84
thahira97 <thahirasheerin3@gmail.com>
2023-03-03 00:57:49 -0500

    Increment the quantity when the button is pressed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,10 @@
 // Client facing scripts here
+
+$(document).ready(function () {
+  $(".increment").click(function () {
+    const input = $(this).siblings("input");
+    let currentValue = parseInt(input.val());
+    let newValue = currentValue + 1;
+    input.val(newValue);
+  });
+});
