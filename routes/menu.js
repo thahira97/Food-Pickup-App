b08385f8651b6ecba5878f26cfd6ad2706e5165e
J8051 <nathaniel.jessica@yahoo.ca>
2023-03-03 17:39:33 -0500

    set cookie for user login, applied cookie to menu page

--- routes/menu.js
+++ routes/menu.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const { getMenu } = require("../db/queries/menu_queries");
 
 router.get("/", (req, res) => {
-  // const clientId = 1;
-  const clientId = req.cookies.user;
+  const clientId = req.cookies.userId;
   getMenu()
     .then((response) => {
       res.render("menu", { dishes: response.rows, clientId: clientId });
@@ -13,17 +12,5 @@ router.get("/", (req, res) => {
       res.status(500);
     });
 });
-
-// router.post('/api/order', (req, res) => {
-//   const clientid = 8
-//   addOrderListToDB(clientid)
-//     .then((result) => {
-//       const orderId = result.rows[0].id;
-//       res.status(201).json({ id: orderId });
-//     })
-//     .catch((err) => {
-//       res.status(500).json({ error: err.message });
-//     });
-// });
 
 module.exports = router;
