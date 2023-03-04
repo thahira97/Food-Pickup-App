381b744ef2b65b10190c3254509c9ba76a22c631
Humaira Tasnim <hxtasnim@gmail.com>
2023-03-04 13:11:57 -0500

    Update order status in database

--- routes/orders.js
+++ routes/orders.js
@@ -60,4 +60,50 @@ router.post("/", function(req, res) {
   }
 });
 
+router.post("/", function(req, res) {
+  if (req.body.est_time) {
+    approveOrder(req.body.order_id, req.body.est_time)
+    .then((response) => {
+      getOrders()
+      .then((response) => {
+        res.render("orders", {orders : response.rows});
+      })
+      .catch(err => {
+        res.status(500)
+      });
+    })
+    .catch(err => {
+      res.status(500)
+    });
+  } else if (req.body.reject) {
+    rejectOrder(req.body.order_id)
+    .then((response) => {
+      getOrders()
+      .then((response) => {
+        res.render("orders", {orders : response.rows});
+      })
+      .catch(err => {
+        res.status(500)
+      });
+    })
+    .catch(err => {
+      res.status(500)
+    });
+  } else if (req.body.complete) {
+    completeOrder(req.body.order_id)
+    .then((response) => {
+      getOrders()
+      .then((response) => {
+        res.render("orders", {orders : response.rows});
+      })
+      .catch(err => {
+        res.status(500)
+      });
+    })
+    .catch(err => {
+      res.status(500)
+    });
+  }
+});
+
 module.exports = router;
