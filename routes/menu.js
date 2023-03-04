8bc12d6c8ba12090c5461964e6b011a05af59c80
J8051 <nathaniel.jessica@yahoo.ca>
2023-03-04 14:51:01 -0500

    fix menu link

--- routes/menu.js
+++ routes/menu.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
-  console.log("RES",req.cookies.userId);
-  if (!req.cookies.userId) {
-    return res.status(401).send("Please log in to place an order");
-  }
   res.render('menu');
 });
 
