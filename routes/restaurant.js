db98f6972a2177e22af0a4154eeab811693a13a0
J8051 <nathaniel.jessica@yahoo.ca>
2023-03-03 20:53:36 -0500

    linked login button functionality to resturant and menu page

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router  = express.Router();
 
 router.get('/', (req, res) => {
+  console.log("RES",req.cookies);
+  if (!req.cookies.userId) {
+    res.render('homepage');
+  }
   res.render('restaurant');
  });
 
