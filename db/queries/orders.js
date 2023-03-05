9349f31af83b739d3875d67ed58d208b0e1423d7
Humaira Tasnim <hxtasnim@gmail.com>
2023-03-05 10:49:30 -0500

    Retrieve dishes for each order

--- db/queries/orders.js
+++ db/queries/orders.js
@@ -2,33 +2,39 @@ const db = require('../connection');
 
 const getOrders = () => {
   return db.query(`
-  SELECT orders.id, orders.order_placed, orders.order_approved, orders.order_completed, orders.estimated_time, orders.order_status, clients.name as client
+  SELECT orders.id, orders.client_id, orders.order_placed, orders.order_approved, orders.order_completed, orders.estimated_time, orders.order_status, clients.name as client, order_dishes.quantity, dishes.title as dish, dishes.price
   FROM orders
-  JOIN clients ON clients.id = client_id;`)
+  JOIN order_dishes ON orders.id = order_id
+  JOIN clients ON clients.id = client_id
+  JOIN dishes ON dishes.id = dish_id;
+  `);
 };
 
-const approveOrder = (order_id, est_time) => {
+const approveOrder = (orderId, estTime) => {
   return db.query(`
   UPDATE orders
-  SET order_approved = CURRENT_TIMESTAMP, estimated_time = ${est_time}
-  WHERE orders.id = ${order_id}
-  RETURNING *;`)
+  SET order_approved = CURRENT_TIMESTAMP, estimated_time = ${estTime}
+  WHERE orders.id = ${orderId}
+  RETURNING *;
+  `);
 };
 
-const rejectOrder = (order_id) => {
+const rejectOrder = (orderId) => {
   return db.query(`
   UPDATE orders
   SET order_status = false
-  WHERE orders.id = ${order_id}
-  RETURNING *;`)
+  WHERE orders.id = ${orderId}
+  RETURNING *;
+  `);
 };
 
-const completeOrder = (order_id) => {
+const completeOrder = (orderId) => {
   return db.query(`
   UPDATE orders
   SET order_completed = CURRENT_TIMESTAMP
-  WHERE orders.id = ${order_id}
-  RETURNING *;`)
+  WHERE orders.id = ${orderId}
+  RETURNING *;
+  `);
 };
 
 module.exports = { getOrders, approveOrder, rejectOrder, completeOrder };
--- routes/orders.js
+++ routes/orders.js
@@ -2,59 +2,61 @@ const express = require('express');
 const router  = express.Router();
 const { getOrders, approveOrder, rejectOrder, completeOrder } = require("../db/queries/orders");
 
-router.get("/", (req, res) => {
+const reduceResponeObj = (obj) => {
+  const out = obj.reduce((a, v) => {
+    if (a[v.id]) {
+      a[v.id].dishes.push({name: v.dish, price: v.price, quantity: v.quantity});
+    } else {
+      a[v.id] = v;
+      a[v.id].dishes = [{name: v.dish, price: v.price, quantity: v.quantity}];
+      delete a[v.id].quantity;
+      delete a[v.id].dish;
+      delete a[v.id].price;
+    }
+    return a;
+  }, {});
+  return Object.values(out);
+};
+
+const renderOrders = (req, res) => {
   getOrders()
     .then((response) => {
-      res.render("orders", {orders : response.rows});
+      res.render("orders", {orders : reduceResponeObj(response.rows)});
     })
     .catch(err => {
-      res.status(500)
+      res.status(500);
     });
+};
+
+router.get("/", (req, res) => {
+  renderOrders(req, res);
 });
 
 router.post("/", function(req, res) {
   if (req.body.est_time) {
     approveOrder(req.body.order_id, req.body.est_time)
-    .then((response) => {
-      getOrders()
       .then((response) => {
-        res.render("orders", {orders : response.rows});
+        renderOrders(req, res);
       })
       .catch(err => {
-        res.status(500)
+        res.status(500);
       });
-    })
-    .catch(err => {
-      res.status(500)
-    });
   } else if (req.body.reject) {
     rejectOrder(req.body.order_id)
-    .then((response) => {
-      getOrders()
       .then((response) => {
-        res.render("orders", {orders : response.rows});
+        renderOrders(req, res);
       })
       .catch(err => {
-        res.status(500)
+        res.status(500);
       });
-    })
-    .catch(err => {
-      res.status(500)
-    });
   } else if (req.body.complete) {
     completeOrder(req.body.order_id)
-    .then((response) => {
-      getOrders()
       .then((response) => {
-        res.render("orders", {orders : response.rows});
+        renderOrders(req, res);
       })
       .catch(err => {
-        res.status(500)
+        res.status(500);
       });
-    })
-    .catch(err => {
-      res.status(500)
-    });
   }
 });
 
