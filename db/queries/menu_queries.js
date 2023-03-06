51eac1a145bb96026ab1febf7a7b34cea4c7af13
thahira97 <thahirasheerin3@gmail.com>
2023-03-05 21:32:45 -0500

    Add INSERT SQL for two tables at a time

--- db/queries/menu_queries.js
+++ db/queries/menu_queries.js
@@ -6,10 +6,20 @@ const getMenu = () => {
 
 const addOrderListToDB = (client_id) => {
   return db
-     .query(`INSERT INTO orders (
-    client_id,
-    order_placed
-  ) VALUES ($1, NOW());`, [9])
+     .query(`
+
+     BEGIN;
+
+     WITH inserted_row AS (
+
+     INSERT INTO orders (client_id, order_placed)
+     VALUES (6, NOW())
+     RETURNING id
+     )
+     INSERT INTO order_dishes ( order_id, dish_id, quantity)
+         VALUES ((SELECT id FROM inserted_row), 4, 5);
+
+     COMMIT;`)
 
 }
 
