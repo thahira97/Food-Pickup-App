a3d608f3e20844ba9bf8de35c3ea35130d4fb622
thahira97 <thahirasheerin3@gmail.com>
2023-03-08 13:27:50 -0500

    Major change in login Backend to check with DB

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const checkMemberId = require("../db/login-database");
+const checkUserEmail = require("../db/login-database");
 
-router.post('/', (req, res) => {
-  checkMemberId(req.body.idVal)
-    .then((result) => {
-      res.cookie("userId", result.id);
-      res.json(result.id);
-    })
-    .catch((err) => { res.json(err); });
+router.post("/", (req, res) => {
+  console.log(req.body.email);
+  checkUserEmail(req.body.email).then((result) => {
+    console.log("result from db", result.rows[0]);
+    const userId = result.rows[0].id;
+    const userEmail = result.rows[0].email;
+
+    // res.clearCookie("userId");
+
+    console.log("is it same", userEmail === "maestro@gmail.com");
+
+    // res.redirect(301,"/orders")
+    if (userEmail === "maestro@gmail.com") {
+      console.log("sdfsdfsdasss", result.rows[0].email);
+      res.cookie("userId", userId);
+      res.redirect("/orders");
+    } else {
+      res.cookie("userId", userId);
+      console.log("reasasss", result.rows[0].email);
+      res.redirect("/menu");
+    }
+  });
+  // .catch(error => {
+
+  //   console.error(error);
+  //   res.status(500).send("Error while checking user email");
+  // });
 });
-
 
 module.exports = router;
