dcdb184089216787bb45877a35950e38b88c4019
J8051 <nathaniel.jessica@yahoo.ca>
2023-03-08 15:36:44 -0500

    added an order total

--- public/scripts/summary.js
+++ public/scripts/summary.js
@@ -1,46 +1,42 @@
 // Client facing scripts here
 
 ///Function to create html of summary
-const createOrderSummary = function(orderItem) {
+const createOrderElement = function(orderItem) {
+
   let $order = `
   <div class="order-list-item">
-    <div class="order-item">
+    <span class="order-item">
      <i class="bi bi-${orderItem.quantity
     }-circle-fill item-count" id="item-name"></i> ${orderItem.title}
-    </div>
-    <div class="order-price">
-     <span>$<strong >${parseFloat(orderItem.price).toFixed(2)}</strong>
+    </span>
+    <span class="order-price">
+     <span>$${parseFloat(orderItem.price).toFixed(2)}
      </span>
-    </div>
+    </span>
 </div>`;
   return $order;
 };
 
 ////Function to render the orderlist html on the order summary
  const renderSummary = function(orderList) {
-
+   let total = 0;
   const $summaryContainer = $("#summary-container");
 
-  for (const order of orderList) {
-    const $summaryHtml = createOrderSummary(order);
+   for (const order of orderList) {
+     total += order.price * order.quantity;
+    const $summaryHtml = createOrderElement(order);
     $summaryContainer.prepend($summaryHtml);
-  }
+   }
+   console.log(total);
+   $("#order-total").text(total);
 };
 
 $(document).ready(function() {
-  $.get("http://localhost:8080/order-summary/get-all-orders", function(data) {
+  $.get("/order-summary/get-all-orders", function(data) {
 
     const { clientId, dishes } = data;
-    console.log(dishes);
+    // console.log(dishes);
     renderSummary(dishes);
   });
-
-
-
-
-
-
-
-
 
 });
