eb7bfc0501303291589d1dfa35c26c7100054129
thahira97 <thahirasheerin3@gmail.com>
2023-03-08 14:01:44 -0500

    Show authorization error if not in DB or not entering email

--- routes/login.js
+++ routes/login.js
@@ -4,31 +4,28 @@ const checkUserEmail = require("../db/login-database");
 
 router.post("/", (req, res) => {
   console.log(req.body.email);
-  checkUserEmail(req.body.email).then((result) => {
-    console.log("result from db", result.rows[0]);
-    const userId = result.rows[0].id;
-    const userEmail = result.rows[0].email;
+  if (!req.body.email) {
+    res.status(401).send("Authorization Error");
+  } else {
+    checkUserEmail(req.body.email)
+      .then((result) => {
+        const userEmail = result.rows[0].email;
+        const userId = result.rows[0].id;
+        // res.clearCookie("userId");
 
-    // res.clearCookie("userId");
-
-    console.log("is it same", userEmail === "maestro@gmail.com");
-
-    // res.redirect(301,"/orders")
-    if (userEmail === "maestro@gmail.com") {
-      console.log("sdfsdfsdasss", result.rows[0].email);
-      res.cookie("userId", userId);
-      res.redirect("/orders");
-    } else {
-      res.cookie("userId", userId);
-      console.log("reasasss", result.rows[0].email);
-      res.redirect("/menu");
-    }
-  });
-  // .catch(error => {
-
-  //   console.error(error);
-  //   res.status(500).send("Error while checking user email");
-  // });
+        if (userEmail === "maestro@gmail.com") {
+          res.cookie("userId", userId);
+          res.redirect("/orders");
+        } else {
+          res.cookie("userId", userId);
+          res.redirect("/menu");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("Error while checking user email");
+      });
+  }
 });
 
 module.exports = router;
