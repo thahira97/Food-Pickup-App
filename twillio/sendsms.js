6cc1ec52d2cddff940d0182f843f747954eaec0e
J8051 <nathaniel.jessica@yahoo.ca>
2023-03-09 10:31:30 -0500

    fix spacing

--- twillio/sendsms.js
+++ twillio/sendsms.js
@@ -1,25 +1,17 @@
 const twilio = require('twilio');
 require('dotenv').config();
 
-const accountSid = process.env.TWILLIO_SID; // Your Account SID from www.twilio.com/console
-const authToken = process.env.AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
+const accountSid = process.env.TWILIO_ACCOUNT_SID // Your Account SID from www.twilio.com/console
+const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
 
 const client = require('twilio')(accountSid, authToken);
 
-const orderPlaced = function() {
-  client.messages
-  .create({
-    body: 'A new order has been placed',
-    to: process.env.CLIENT_PH_NUM, // Text this number
-    from: process.env.TWILLIO_PH_NUM, // From a valid Twilio number
-  })
-  .then((message) => console.log(message.sid));
-};
+
 
 const orderCreated = function() {
   client.messages
   .create({
-    body: 'Your order will be ready in 20 mins',
+    body: 'Your order is being prepared',
     to: process.env.CLIENT_PH_NUM, // Text this number
     from: process.env.TWILLIO_PH_NUM, // From a valid Twilio number
   })
@@ -37,3 +29,4 @@ const orderReady = function() {
   .then((message) => console.log(message.sid));
 };
 
+module.exports = {orderReady, orderCreated};
